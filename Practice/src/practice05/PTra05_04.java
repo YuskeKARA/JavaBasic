d47ba6c415b7e03033df86870f88a99773f4582a
YuskeKARA <YuskeKARA@users.noreply.github.com>
2019-02-13 18:31:23 +0900

    Practice05-04

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -23,16 +23,43 @@ public class PTra05_04 {
 		 */
 
 		// ①
-		System.out.println("0～6の整数を入力してください");
+		boolean loopFlg = true ;
+		while(loopFlg) {
+			System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+			String line = scanner.nextLine();
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
+			switch(line) {
+				case "0":
+					System.out.println("日曜");
+					loopFlg=false;
+					break;
+				case "1":
+					System.out.println("月曜");
+					loopFlg=false;
+					break;
+				case "2":
+					System.out.println("火曜");
+					loopFlg=false;
+					break;
+				case"3":
+					System.out.println("水曜");
+					loopFlg=false;
+					break;
+				case"4":
+					System.out.println("木曜");
+					loopFlg=false;
+					break;
+				case"5":
+					System.out.println("金曜");
+					loopFlg=false;
+					break;
+				case"6":
+					System.out.println("土曜");
+					loopFlg=false;
+					break;
+				default:
+			}
 		}
 	}
 }
