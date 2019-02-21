e5065876009982030bec16cb44ae34852101bb2b
YuskeKARA <YuskeKARA@users.noreply.github.com>
2019-02-21 18:31:16 +0900

    Update Ptractice13_09

--- Practice/src/practice13/ptra13/PTra13_09.java
+++ Practice/src/practice13/ptra13/PTra13_09.java
@@ -6,6 +6,8 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Employee;
+
 public class PTra13_09 {
 
 	/*
@@ -34,6 +36,34 @@ public class PTra13_09 {
 	public static void main(String[] args) {
 
 		// ★ 定数で定義されている各データを使用して、Employeeインスタンスを３つ作成してください
+		Employee employee1 = new Employee();
+		employee1.setUserNm("山田");
+		System.out.println(employee1.getUserNm());
+		employee1.setMail("yamada@hoge.com");
+		System.out.println(employee1.getMail());
+		employee1.setPassword("rezo0001");
+		System.out.println(employee1.getPassword());
+		employee1.setDepartmentNm("総務部");
+		System.out.println(employee1.getDepartmentNm());
 
+		Employee employee2 = new Employee();
+		employee2.setUserNm("佐藤");
+		System.out.println(employee2.getUserNm());
+		employee2.setMail("satou@hoge.com");
+		System.out.println(employee2.getMail());
+		employee2.setPassword("rezo0002");
+		System.out.println(employee2.getPassword());
+		employee2.setDepartmentNm("業務部");
+		System.out.println(employee2.getDepartmentNm());
+
+		Employee employee3 = new Employee();
+		employee3.setUserNm("小林");
+		System.out.println(employee3.getUserNm());
+		employee3.setMail("kobayashi@hoge.com");
+		System.out.println(employee3.getMail());
+		employee3.setPassword("rezo0003");
+		System.out.println(employee3.getPassword());
+		employee3.setDepartmentNm("システム部");
+		System.out.println(employee3.getDepartmentNm());
 	}
 }
